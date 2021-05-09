2b54ffc83ebeceac5917a942542e5f06ef796227
KawaiAkihiro <72666840+KawaiAkihiro@users.noreply.github.com>
2021-05-09 14:38:01 +0900

    テンプレート文字列

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
-import "./styles.css";
+/**
+ *  const, letについて
+ */
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// var val1 = "var変数"
+// console.log(val1)
+
+// // var変数は上書き可能
+// val1 = "var変数を上書き"
+// console.log(val1)
+
+// var変数は再宣言可能
+// var val1 = "var変数を再宣言"
+// console.log(val1)
+
+//テンプレート文字列
+const name = "kawai";
+const age = 23;
+const message = `私の名前は${name}です。年齢は${age}です。`;
+console.log(message);
