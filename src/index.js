18047d365a920759dc21002ab5090fcfba19bcaf
KawaiAkihiro <72666840+KawaiAkihiro@users.noreply.github.com>
2021-05-09 14:45:44 +0900

    アロー関数

--- src/index.js
+++ src/index.js
@@ -13,8 +13,28 @@
 // var val1 = "var変数を再宣言"
 // console.log(val1)
 
-//テンプレート文字列
-const name = "kawai";
-const age = 23;
-const message = `私の名前は${name}です。年齢は${age}です。`;
-console.log(message);
+// //テンプレート文字列
+// const name = "kawai";
+// const age = 23;
+// const message = `私の名前は${name}です。年齢は${age}です。`;
+// console.log(message);
+
+//アロー関数
+//従来
+
+// function func1(str){
+//   console.log(str)
+// }
+
+// func1("akihiro")
+
+//アロー関数
+// const func2 = (str) =>  str;
+
+// console.log(func2("kawai"));
+
+// const func3 = (x,y) => {
+//   return x+y;
+// }
+
+// console.log(func3())
